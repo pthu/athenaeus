be87e1c90753f6b80cdc360c54f6a37e0c29422f
ernstboogert <ernstboogert@dadel.org>
2019-07-10 13:05:04 +0200

    change directory structure

---
+++ Athenaeus/Deipnosophistae/tf/1.0/_book.tf
@@ -0,0 +1,25 @@
+@node
+@_book=Deipnosophistae
+@author=Athenaeus
+@availableStructure=book,chapter
+@compiler=Ernst Boogert
+@description=the title of the book
+@edition=Athenaeus, Deipnosophistae, Georg Kaibel, In Aedibus B.G. Teubneri, Lipsiae, 1887.
+@editor=Georg Kaibel
+@institution=Protestant Theological University (PThU) Amsterdam/Groningen, The Netherlands
+@lang=grc
+@license=not provided by source
+@name=Patristic corpus from Perseus
+@purpose=Making Perseus TEI formatted text available in TF
+@source1=Perseus Digital Library
+@source2=Open Greek and Latin Project
+@status=initial unchecked conversion
+@url1=https://github.com/PerseusDL/canonical-greekLit
+@url2=https://github.com/OpenGreekAndLatin/First1KGreek
+@urn=urn:cts:greekLit:tlg0008.tlg001.perseus-grc3
+@valueType=str
+@version=1.0
+@writtenBy=Text-Fabric
+@dateWritten=2019-07-10T11:04:27Z
+
+265169	Deipnosophistae
---
+++ Athenaeus/Deipnosophistae/tf/1.0/otext.tf
@@ -0,0 +1,33 @@
+@config
+@_book=Deipnosophistae
+@author=Athenaeus
+@availableStructure=book,chapter
+@compiler=Ernst Boogert
+@edition=Athenaeus, Deipnosophistae, Georg Kaibel, In Aedibus B.G. Teubneri, Lipsiae, 1887.
+@editor=Georg Kaibel
+@fmt:text-orig-beta-plain={beta_plain} 
+@fmt:text-orig-full={orig} 
+@fmt:text-orig-lemma={lemma} 
+@fmt:text-orig-main={main} 
+@fmt:text-orig-norm={norm} 
+@fmt:text-orig-plain={plain} 
+@institution=Protestant Theological University (PThU) Amsterdam/Groningen, The Netherlands
+@lang=grc
+@license=not provided by source
+@name=Patristic corpus from Perseus
+@purpose=Making Perseus TEI formatted text available in TF
+@sectionFeatures=_book,book,chapter
+@sectionTypes=_book,book,chapter
+@source1=Perseus Digital Library
+@source2=Open Greek and Latin Project
+@status=initial unchecked conversion
+@structureFeatures=_book,book,chapter
+@structureTypes=_book,book,chapter
+@url1=https://github.com/PerseusDL/canonical-greekLit
+@url2=https://github.com/OpenGreekAndLatin/First1KGreek
+@urn=urn:cts:greekLit:tlg0008.tlg001.perseus-grc3
+@valueType=int
+@version=1.0
+@writtenBy=Text-Fabric
+@dateWritten=2019-07-10T11:04:31Z
+
