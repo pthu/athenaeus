54bedd613b5e8c79e0b5bd86a2275ef3d2a3176b
ernstboogert <ernstboogert@dadel.org>
2019-07-10 12:52:58 +0200

    release 1.0 of Athenaeus

---
+++ tf/1.0/Athenaeus/Deipnosophistae/otype.tf
@@ -0,0 +1,38 @@
+@node
+@_book=Deipnosophistae
+@author=Athenaeus
+@availableStructure=book,chapter
+@compiler=Ernst Boogert
+@edition=Athenaeus, Deipnosophistae, Georg Kaibel, In Aedibus B.G. Teubneri, Lipsiae, 1887.
+@editor=Georg Kaibel
+@institution=Protestant Theological University (PThU) Amsterdam/Groningen, The Netherlands
+@lang=grc
+@license=not provided by source
+@name=Patristic corpus from Perseus
+@purpose=Making Perseus TEI formatted text available in TF
+@source1=Perseus Digital Library
+@source2=Open Greek and Latin Project
+@status=initial unchecked conversion
+@url1=https://github.com/PerseusDL/canonical-greekLit
+@url2=https://github.com/OpenGreekAndLatin/First1KGreek
+@urn=urn:cts:greekLit:tlg0008.tlg001.perseus-grc3
+@valueType=str
+@version=1.0
+@writtenBy=Text-Fabric
+@dateWritten=2019-07-10T10:51:29Z
+
+1-265168	word
+265169	_book
+265170-277955	_sentence
+277956-278744	add
+278745-284008	bibl
+284009-284023	book
+284024-285351	chapter
+285352-285518	cit
+285519	head
+285520-285597	hi
+285598-296864	l
+296865-297139	num
+297140-298710	p
+298711-300259	pb
+300260-303385	quote
