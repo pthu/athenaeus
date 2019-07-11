1a0705238d04878cbf9ef2c51fb893701ffabd06
ernstboogert <ernstboogert@dadel.org>
2019-07-11 16:31:32 +0200

    problem with _sentence feature solved

---
+++ Athenaeus/Deipnosophistae/tf/1.1/otype.tf
@@ -0,0 +1,38 @@
+@node
+@_book=Deipnosophistae
+@author=Athenaeus
+@availableStructure=book,chapter
+@compiler=Ernst Boogert
+@edition=Athenaeus, Deipnosophistae, Georg Kaibel, In Aedibus B.G. Teubneri, Lipsiae, 1887.
+@editor=Georg Kaibel
+@institution=Protestant Theological University (PThU) Amsterdam/Groningen, The Netherlands
+@lang=grc
+@license=not provided by source
+@name=Patristic corpus from Perseus
+@purpose=Making Perseus TEI formatted text available in TF
+@source1=Perseus Digital Library
+@source2=Open Greek and Latin Project
+@status=initial unchecked conversion
+@url1=https://github.com/PerseusDL/canonical-greekLit
+@url2=https://github.com/OpenGreekAndLatin/First1KGreek
+@urn=urn:cts:greekLit:tlg0008.tlg001.perseus-grc3
+@valueType=str
+@version=1.0
+@writtenBy=Text-Fabric
+@dateWritten=2019-07-11T14:30:28Z
+
+1-265146	word
+265147	_book
+265148-279920	_sentence
+279921-280709	add
+280710-285973	bibl
+285974-285988	book
+285989-287316	chapter
+287317-287483	cit
+287484	head
+287485-287562	hi
+287563-298829	l
+298830-299104	num
+299105-300675	p
+300676-302224	pb
+302225-305350	quote
